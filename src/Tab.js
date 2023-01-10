97b2de916f7aa718a67eae161dfd6b11180949b8
hungvu <hungvu@fortna.com>
2023-01-10 17:18:50 +0700

    update

--- src/Tab.js
+++ src/Tab.js
@@ -2,23 +2,37 @@ import React, { useState } from 'react';
 import styles from './App.module.scss';
 
 export default function Tab(props) {
-  const { labelType, children, defaultActiveKey = 0 } = props;
+  const { children, defaultActiveKey = 0, renderTabBar, tabContentClass, onChangeTab, tabType } = props;
   const [activeTab, setActiveTab] = useState(defaultActiveKey);
   const isArray = Array.isArray(children);
+  const vertical = tabType === 'vertical';
+
+  const onHandleChangeTab = (ind) => () => {
+    setActiveTab(ind);
+    onChangeTab(ind);
+  };
 
   const renderTabOption = (label, i) => {
     return (
-      <div key={i} className={`${styles.tabOptionItem} ${activeTab === i && styles.active}`} onClick={() => setActiveTab(i)}>{label}</div>
+      <div
+        key={i}
+        className={`${styles.tabOptionItem} ${activeTab === i && styles.active}`}
+        onClick={onHandleChangeTab(i)}>
+        <div className={styles.txtLabel}>{label}</div>
+      </div>
     );
   };
+
   return (
-    <div>
+    <div className={`${styles.tabWrapper} ${vertical && styles.vertical}`}>
       <div className={styles.tabOption}>
         {isArray
-          ? children?.map((child, i) => renderTabOption(child.props.tabLabel, i))
+          ? children?.map((child, ind) => renderTabBar?.({
+            label: child.props.tabLabel, index: ind, setActiveTab, activeTab
+          }) || renderTabOption(child.props.tabLabel, ind))
           : renderTabOption(children.props.tabLabel, 1)}
       </div>
-      <div>
+      <div className={`${styles.tabContent} ${tabContentClass}`}>
         {isArray ? children?.[activeTab] : children}
       </div>
     </div>
